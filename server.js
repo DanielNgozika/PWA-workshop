3fd2589b87098863bf31c93d39e5c6323d074d36
Daniel Ngozika <danielngozikac@gmail.com>
2019-11-24 12:38:50 +0100

    Create server.

---
+++ server.js
@@ -0,0 +1,29 @@
+// import http from "http";
+// import express from "express";
+// const app = express();
+
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: "it works!"
+//     })
+// })
+
+// const port = process.env.PORT || 4000;
+
+// const server = http.createServer(app);
+
+// server.listen(port);
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(process.env.PORT || 8080);
