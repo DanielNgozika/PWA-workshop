cbcf484ccb3776a23d738a9044bebc88916356f0
Daniel Ngozika <danielngozikac@gmail.com>
2020-01-20 19:48:48 +0100

    Listen to new events on the serviceWorker.

--- public/sw.js
+++ public/sw.js
@@ -13,7 +13,7 @@ self.addEventListener("activate", event => console.log("Activate event"));
 // the cached resources searched for if available or lets the request
 // continue to the server if not.
 self.addEventListener("fetch", event =>
-	event.respondWIth(
+	event.respondWith(
 		caches.match(event.request).then(cachedResponse => {
 			return cachedResponse || fetch(event.request);
 		})
@@ -22,3 +22,34 @@ self.addEventListener("fetch", event =>
 
 const cacheName = "cache-v1";
 const toBePrecached = ["/", "index.html", "styles/main.css"];
+
+//Handling push notification
+self.addEventListener("push", event => {
+	if (event.data.text == "new DM") {
+		event.waitUntil(
+			caches.open("cache-v1").then(cache => {
+				return fetch("/inbox.json").then(res => {
+					cache.put("/inbox.json", res.clone());
+					return res.json()
+				});
+			}).then(showDMNotification)
+		);
+	}
+});
+
+const showEmailNotification = DMs => {
+	registration.showNotification("New DM", {
+		body: `From ${DMs[0].from.name}`,
+		tag: "new tweet"
+	});
+}
+
+self.addEventListener("notificationclick", event => {
+	if (event.notification.tag == "new DM") {
+		//Assuming that resources needed to render 
+		//inbox have already been cached
+		// (during install event handling, maybe)
+
+		new WindowClient('/inbox')
+	}
+})
